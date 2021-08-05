9ee519ae042554f65c45cf42f62e54c5aa0a717b
이소민 <somin116@gmail.com>
2021-08-05 14:09:45 +0900

    좋아요/나빠요 버튼 누를 때 처리 구현

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Question.scss";
 import InputBox from "./InputBox";
+// import { ReactComponent as GoodIcon } from "../../public/images/ic-mydocs-good.svg";
 
 function Question(props) {
   const { questionText, answerText } = { ...props };
+  const [isThumbClicked, setIsThumbClicked] = useState("x");
+
+  function goodOnClick() {
+    if (isThumbClicked == "g") setIsThumbClicked("x");
+    else setIsThumbClicked("g");
+  }
+
+  function badOnClick() {
+    if (isThumbClicked == "b") setIsThumbClicked("x");
+    else setIsThumbClicked("b");
+  }
 
   return (
     <div>
@@ -17,11 +29,29 @@ function Question(props) {
       <div id="underQuestion">
         <span id="evaluateQuestionBox">
           이 질문이 마음에 드셨나요?
-          <img id="icGood" src="/images/ic-mydocs-good.svg"></img>
-          <img id="icBad" src="/images/ic-mydocs-bad.svg"></img>
+          <img
+            id="icGood"
+            src={
+              isThumbClicked == "g"
+                ? "/images/ic-mydocs-good-clicked.svg"
+                : "/images/ic-mydocs-good.svg"
+            }
+            style={{ height: "9px", width: "9.4px" }}
+            onClick={goodOnClick}
+          />
+          <img
+            id="icBad"
+            src={
+              isThumbClicked == "b"
+                ? "/images/ic-mydocs-bad-clicked.svg"
+                : "/images/ic-mydocs-bad.svg"
+            }
+            style={{ height: "9px", width: "9.4px" }}
+            onClick={badOnClick}
+          />
         </span>
         <span id="editAnswerBox">
-          <img id="icEdit" src="/images/ic-mydocs-g-write.svg"></img>
+          <img id="icEdit" src="/images/ic-mydocs-g-write.svg" />
           수정
         </span>
       </div>
