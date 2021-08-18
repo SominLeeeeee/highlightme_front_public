3f23ee30d1c0e10a9d7ba75c76199d49ff55dbd8
이소민 <somin116@gmail.com>
2021-08-18 16:27:10 +0900

    findQuestion 생긴거 완성

--- src/routes/QuestionsList.js
+++ src/routes/QuestionsList.js
@@ -12,23 +12,25 @@ function QuestionsList() {
     {
       title: "React에서 redux를 사용해보신 적 있나요?",
     },
+    {
+      title: "React에서 redux를 사용해보신 적 있나요오오오오오오오오옹?",
+    },
+    {
+      title:
+        "React에서 redux를 사용해보신 적 있나아아아아아아아아아아아아아아아아아아아아아아아아아아요?",
+    },
   ];
   // TODO: keyword에 대한 질문 받아오기
 
   return (
     <div className="questionsListParent">
-      <div className="questionsListParent2">
-        <HighlightText
-          text="예상질문 리스트"
-          marginBottom="1.75rem"
-        ></HighlightText>
-        <ShadowBoxMedium paddingTop="4.8rem">
-          <p id="keywordName">{keyword}</p>
-          {arr.map((element) => (
-            <Question questionText={element.title} />
-          ))}
-        </ShadowBoxMedium>
-      </div>
+      <HighlightText text="예상질문 리스트" marginBottom="3.6rem" />
+      <ShadowBoxMedium paddingTop="4.8rem">
+        <p id="keywordName">{keyword}</p>
+        {arr.map((element) => (
+          <Question questionText={element.title} />
+        ))}
+      </ShadowBoxMedium>
     </div>
   );
 }
