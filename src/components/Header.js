044ec5a32c662aaadf53fd88da808c5b7bf06ec8
이소민 <somin116@gmail.com>
2021-09-02 17:51:17 +0900

    [FE-8] flex로 정렬 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,15 @@ import React from "react";
 function Header() {
   return (
     <div id="headerWrapper">
-      <img src="/images/ic-sign-logo.svg" />
-      <p>질문찾기</p>
-      <p>모아보기</p>
-      <p>주고받기</p>
-      <img src="/images/ic-profile.svg" />
+      <div id="header">
+        <img src="/images/ic-sign-logo.svg" />
+        <span id="headerNavigators">
+          <p>질문찾기</p>
+          <p>모아보기</p>
+          <p>주고받기</p>
+          <img src="/images/ic-profile.svg" />
+        </span>
+      </div>
     </div>
   );
 }
