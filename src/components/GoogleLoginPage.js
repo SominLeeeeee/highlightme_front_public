691a68b66e1dc30e82a3f70810ac67747b468fe4
이소민 <somin116@gmail.com>
2021-09-06 11:50:58 +0900

    [FE-4] 로그인 버튼 디자인 적용

--- src/components/GoogleLoginPage.js
+++ src/components/GoogleLoginPage.js
@@ -2,16 +2,37 @@ import React from "react";
 import { GoogleLogin } from "react-google-login";
 
 function GoogleLoginPage() {
+  function attachSignin() {}
+
   const responseGoogle = (response) => {
     console.log(response);
   };
 
+  const googleLoginButtonStyle = {
+    backgroundColor: "white",
+    borderRadius: "3.2rem",
+    border: "none",
+    boxShadow: "0rem 0.4rem 0.8rem #0000001A",
+    fontFamily: "AppleSDGothicNeo-Bold",
+    fontSize: "20px",
+    padding: "2rem 3rem",
+    display: "flex",
+    alignItems: "center",
+  };
+
   return (
     <div>
-      <h1>google login test</h1>
       <GoogleLogin
         clientId="568158562597-qu7pvd53laqmvfsas5bihd5k1lk53c3s.apps.googleusercontent.com"
-        buttonText="GoogleLogin"
+        render={(renderProps) => (
+          <button style={googleLoginButtonStyle} onClick={renderProps.onClick}>
+            <img
+              src="/images/btn-google-signin.png"
+              style={{ width: "24px", height: "24px", marginRight: "8px" }}
+            ></img>
+            구글 계정으로 간편하게 시작하기
+          </button>
+        )}
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
         cookiePolicy={"single_host_origin"}
