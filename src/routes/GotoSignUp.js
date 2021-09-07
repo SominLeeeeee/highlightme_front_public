3e5b0f06788c54e69f98a0da3bfafebcb482c4b4
이소민 <somin116@gmail.com>
2021-09-07 16:34:08 +0900

    [FE-3] 여백 조정

--- src/routes/GotoSignUp.js
+++ src/routes/GotoSignUp.js
@@ -4,18 +4,20 @@ import "../style/gotoSignUp.scss";
 
 function GotoSignUp() {
   return (
-    <div>
-      <img src="/images/ic-quotation.svg" />
-      <p>
-        자기소개서로
-        <br />
-        나에게 맞는 질문을
-        <br />
-        예상 받아보세요
-      </p>
-      <p>이제 면접 대비는 하이라이트미</p>
-      <GoogleLoginPage></GoogleLoginPage>
-      <p>
+    <div className="gotoSignUpWrapper">
+      <div className="gotoSignUp">
+        <img id="signupQuotation" src="/images/ic-quotation.svg" />
+        <p id="signupPhrase1">
+          자기소개서로
+          <br />
+          나에게 맞는 질문을
+          <br />
+          예상 받아보세요
+        </p>
+        <p id="signupPhrase2">이제 면접 대비는 하이라이트미</p>
+        <GoogleLoginPage />
+      </div>
+      <p id="signupAgree">
         간편하게 시작하기를 통해 <b>서비스 이용약관</b>과{" "}
         <b>개인정보처리방침</b>에 동의합니다.
       </p>
