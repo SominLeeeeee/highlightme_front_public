52764da8647005b9879686410508b61f4afb1a00
이소민 <somin116@gmail.com>
2021-09-15 14:21:39 +0900

    [FE-3] redux store에서 메일 주소 불러와서 표시

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -6,13 +6,14 @@ import HighlightButton from "../components/HighlightButton";
 import ShadowBoxLarge from "../components/ShadowBoxLarge";
 import Asterisk from "../components/Asterisk";
 import SelectedFieldJob from "../components/SelectedFieldJob";
+import { useSelector } from "react-redux";
 
 function SignUp(props) {
-  const { email } = { ...props };
   const [fieldSelected, setFieldSelected] = useState(0); // 분야
   const [jobSelected, setJobSelected] = useState(0); // 직무
   const [fieldJob, setFieldJob] = useState([]); // 선택한 분야와 직무
 
+  const email = useSelector((state) => state.email);
   const fieldArr = ["분야를 선택해주세요", "IT / 컴퓨터", "디자인", "c"];
   const jobArr = [
     ["직무를 선택해주세요."],
@@ -29,7 +30,6 @@ function SignUp(props) {
 
   function jobOnChange(e) {
     setJobSelected(e.target.selectedIndex);
-
     setFieldJob(
       fieldJob.concat({
         field: fieldArr[fieldSelected],
