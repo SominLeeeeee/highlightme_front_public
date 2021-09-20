9cb75a34d7debb6805ce02725fd7984efa8f5de6
이소민 <somin116@gmail.com>
2021-09-20 17:25:36 +0900

    [FE-10] 사용자 정보, 키워드, 자기소개서 관련 atom 생성

--- src/recoil/userStore.js
+++ src/recoil/userStore.js
@@ -2,5 +2,23 @@ import { atom } from "recoil";
 
 export const atomCoverLetterElements = atom({
   key: "atomCoverLetterElements",
-  default: [{ problem: "자기소개서 문항을 입력해주세요", answer: "" }],
+  default: {
+    element: [{ problem: "자기소개서 문항을 입력해주세요", answer: "" }],
+    selectedElement: "0",
+  },
+});
+
+export const atomUserInfo = atom({
+  key: "atomUserInfo",
+  default: {
+    email: "",
+  },
+});
+
+export const atomKeyword = atom({
+  key: "atomKeyword",
+  default: {
+    userKeywords: [],
+    selectedKeyword: "",
+  },
 });
