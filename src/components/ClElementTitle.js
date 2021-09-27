e19173fb94c74f6beeab281fb2f982e8d0ded2d9
이소민 <somin116@gmail.com>
2021-09-27 14:34:38 +0900

    [FE-2] 제목 리스트에서 선택한 문항 강조 표시

--- src/components/ClElementTitle.js
+++ src/components/ClElementTitle.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { atomCoverLetterElements } from "../recoil/userStore";
 import "./clElementTitle.scss";
 
 function ClElementTitle(props) {
   const { number, problem } = { ...props };
+  const [coverLetterElements, setCoverLetterElements] = useRecoilState(
+    atomCoverLetterElements
+  );
+
+  function clElementTitleOnClick() {
+    setCoverLetterElements((prev) => ({
+      ...prev,
+      selectedElement: number - 1,
+    }));
+  }
+
   return (
-    <div className="clElementTitle">
+    <div
+      className={
+        number - 1 === coverLetterElements.selectedElement
+          ? "clElementTitleActive"
+          : "clElementTitleDefault"
+      }
+      onClick={clElementTitleOnClick}
+    >
       <div className="clElementNumber">{number}</div>
       <p>{problem}</p>
     </div>
