6c3d2b20515a5ff5493f7712b2bcb0ec659d9515
이소민 <somin116@gmail.com>
2021-09-30 17:58:59 +0900

    [FE-11] 불필요한 코드 정리

--- src/components/SignUp/GoogleLoginButton.js
+++ src/components/SignUp/GoogleLoginButton.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { GoogleLogin } from "react-google-login";
 import config from "../../configs";
-import { Redirect, useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { atomSignUp, atomUserInfo } from "../../recoil/userStore";
-import produce from "immer";
 
 function GoogleLoginButton() {
   const history = useHistory();
-  const dispatch = useDispatch();
-  const [userInfo, setUserInfo] = useRecoilState(atomUserInfo);
   const [signUp, setSignUp] = useRecoilState(atomSignUp);
 
   const googleBtnOnClick = async (response) => {
@@ -30,22 +26,9 @@ function GoogleLoginButton() {
       });
 
     console.log("회원가입 결과", alreadySignUp);
-    // dispatch({ type: SIGN_UP, email });
     setSignUp({ signUpLevel: 0 });
     history.push("/signup_info");
   };
-
-  // const googleBtnOnClick = async () => {
-  //   console.log("aaa");
-  //   const alreadySignUp = await fetch(`${config.URL}/api/users/oauth/google`, {
-  //     method: "GET",
-  //   });
-
-  //   console.log(alreadySignUp);
-  //   // dispatch({ type: SIGN_UP, email });
-  //   dispatch({ type: SIGN_UP_REGISTER, level: 0 });
-  //   history.push("/signup_info");
-  // };
 
   const googleLoginButtonStyle = {
     backgroundColor: "white",
@@ -77,13 +60,6 @@ function GoogleLoginButton() {
       cookiePolicy={"single_host_origin"}
       // isSignedIn={true}
     />
-    // <button onClick={googleBtnOnClick} style={googleLoginButtonStyle}>
-    //   <img
-    //     src="/images/btn-google-signin.png"
-    //     style={{ width: "24px", height: "24px", marginRight: "8px" }}
-    //   />
-    //   구글 계정으로 간편하게 시작하기
-    // </button>
   );
 }
 
