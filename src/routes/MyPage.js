b70a81959ec2db7a91ef531f73c19916fb8ecde6
1chang1 <aarh4n@gmail.com>
2021-10-08 16:36:28 +0900

    [FE-HEADER] 마이페이지에서 로그인 정보 검증

--- src/routes/MyPage.js
+++ src/routes/MyPage.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router";
 import Header from "../components/Header";
 import "./myPage.scss";
+import { useRecoilState } from "recoil";
+import { atomSignUp, atomUserInfo } from "../recoil/userStore";
+import { isUserValid } from "../utils";
 
 function MyPage() {
+  const history = useHistory();
+  const [userInfo, setUserInfo] = useRecoilState(atomUserInfo);
+
+  useEffect(() => {
+    if (!isUserValid(userInfo)) {
+      history.push("/signup");
+    }
+  }, []);
+
+  const onSignOutClick = () => {
+    history.push("/signout");
+  };
+
   return (
     <div>
       <Header />
@@ -12,7 +29,9 @@ function MyPage() {
             <p id="myPageTitle">마이페이지</p>
             <p id="myPageExplain">임시로 만들어진 마이페이지입니다!</p>
           </div>
-          <div className="keywordQuestionWrapper"></div>
+          <div className="keywordQuestionWrapper">
+            <button onClick={onSignOutClick}>로그아웃</button>
+          </div>
         </div>
       </div>
     </div>
