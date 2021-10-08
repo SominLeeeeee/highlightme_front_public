bf830087b4fc853745fdf4b636f3d97e19319fa2
1chang1 <aarh4n@gmail.com>
2021-10-08 16:04:06 +0900

    [FE-HEADER] 자소서 등록 페이지에 헤더 추가

--- src/routes/Coverletter.js
+++ src/routes/Coverletter.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./coverletter.scss";
 import ClInput from "../components/CoverLetter/ClInput";
 import ClList from "../components/CoverLetter/ClList";
+import Header from "../components/Header";
 
 function Coverletter() {
   return (
-    <div className="coverletterWrapper">
-      <div className="coverletter">
-        <ClList />
-        <ClInput />
+    <div>
+      <Header />
+      <div className="coverletterWrapper">
+        <div className="coverletter">
+          <ClList />
+          <ClInput />
+        </div>
       </div>
     </div>
   );
