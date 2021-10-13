eb64cad13aa873789ae9f62ca90b2e378e009834
이소민 <somin116@gmail.com>
2021-10-13 19:16:16 +0900

    [FE-1] 디자인 디테일 수정

--- src/components/atom/ShadowBoxMedium.js
+++ src/components/atom/ShadowBoxMedium.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./ShadowBoxMedium.scss";
 
 function ShadowBoxMedium(props) {
-  const { paddingTop, children } = { ...props };
+  const { paddingTop, children, paddingBottom } = { ...props };
 
   return (
     <div
       className="shadowBoxMedium"
       style={{
         paddingTop: paddingTop,
+        paddingBottom: paddingBottom,
       }}
     >
       {children}
