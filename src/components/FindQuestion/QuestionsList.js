e02f9ed8c3c265930f143463934bba769456c38c
이소민 <somin116@gmail.com>
2021-10-13 10:30:06 +0900

    [FE-1] 불필요한 코드 삭제 및 주석

--- src/components/FindQuestion/QuestionsList.js
+++ src/components/FindQuestion/QuestionsList.js
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from "react";
 
 import config from "../../configs";
-import { useParams } from "react-router-dom";
 import "./questionsList.scss";
 import Question from "../FindQuestion/Question.js";
 import HighlightText from "../atom/HighlightText";
 import ShadowBoxMedium from "../atom/ShadowBoxMedium";
 import { useRecoilState } from "recoil";
-import { atomUserInfo, atomKeyword } from "../../recoil/userStore";
+import { atomKeyword } from "../../recoil/userStore";
 
 function QuestionsList(props) {
   const [questions, setQuestions] = useState([]);
-  const [userInfo, setUserInfo] = useRecoilState(atomUserInfo);
   const [keyword, setKeyword] = useRecoilState(atomKeyword);
 
-  //41, 53
+  /* 선택한 키워드가 변경되면 해당 키워드의 질문을 가져와서 questions에 저장 */
   useEffect(async () => {
-    let result = await fetch(`${config.URL}/api/questions`, {
+    await fetch(`${config.URL}/api/questions`, {
       method: "POST",
       body: new URLSearchParams({
         user_keyword_id: keyword.selectedKeyword,
@@ -33,22 +31,10 @@ function QuestionsList(props) {
       });
   }, [keyword.selectedKeyword]);
 
+  /* questions 변경되면 로그 띄우기 */
   useEffect(() => {
     console.log("questions", questions);
   }, [questions]);
-  const arr = [
-    {
-      title: "React에서 redux를 사용해보신 적 있나요?",
-    },
-    {
-      title: "React에서 redux를 사용해보신 적 있나요오오오오오오오오옹?",
-    },
-    {
-      title:
-        "React에서 redux를 사용해보신 적 있나아아아아아아아아아아아아아아아아아아아아아아아아아아요?",
-    },
-  ];
-  // TODO: keyword에 대한 질문 받아오기
 
   return (
     <div className="questionsListParent">
