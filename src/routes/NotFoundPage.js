66e8bb49d4b59c5270102b0ac01dfa1c61beeb48
1chang1 <aarh4n@gmail.com>
2021-10-19 13:29:36 +0900

    [FE-15] Add NotFoundPage

---
+++ src/routes/NotFoundPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+function NotFoundPage() {
+  const history = useHistory();
+
+  const onHomeButtonClick = () => {
+    history.push("/");
+  };
+
+  return (
+    <div>
+      <h4>존재하지 않는 페이지에요</h4>
+      <br />
+      <button onClick={onHomeButtonClick}>메인으로 가기</button>
+    </div>
+  );
+}
+
+export default NotFoundPage;
