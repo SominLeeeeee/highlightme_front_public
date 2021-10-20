05e5934eaea71e2d9155061057cd68d77a536f68
이소민 <somin116@gmail.com>
2021-10-20 23:00:31 +0900

    [FE-21] 자기소개서 등록 페이지 접속 시 서버에서 정보 받아오기

--- src/routes/CoverletterPage.js
+++ src/routes/CoverletterPage.js
@@ -4,13 +4,49 @@ import ClInput from "../components/CoverLetter/ClInput";
 import ClList from "../components/CoverLetter/ClList";
 import Header from "../common/Header";
 import { useRecoilState } from "recoil";
-import { atomMenu } from "../recoil/userStore";
+import { atomCoverLetterElements, atomMenu } from "../recoil/userStore";
+import config from "../configs";
+import produce from "immer";
 
 function CoverletterPage() {
   const [menu, setMenu] = useRecoilState(atomMenu);
+  const [cle, setCle] = useRecoilState(atomCoverLetterElements);
 
-  useEffect(() => {
+  /**
+   * 자소서 등록 페이지 처음 접속 시
+   * 서버에서 자기소개서 받아오기
+   */
+  useEffect(async () => {
     setMenu("자기소개서");
+
+    let res;
+
+    try {
+      res = await fetch(`${config.URL}/api/cls`, {
+        method: "GET",
+        credentials: "include",
+      });
+      res = await res.json();
+    } catch (error) {
+      console.log("fail to load coverletter 😭");
+      res = false;
+    }
+
+    if (res) {
+      let fromServerCle = [];
+      res.result.map((element) => {
+        fromServerCle = fromServerCle.concat({
+          problem: element.problem,
+          answer: element.answer,
+        });
+      });
+
+      setCle((prev) => ({
+        ...prev,
+        element: fromServerCle,
+        selectedElement: 0,
+      }));
+    }
   }, []);
 
   return (
