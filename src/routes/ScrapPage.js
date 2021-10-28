2d9e88cc4b8cd18dc12a4b781231a94f252362dc
이소민 <somin116@gmail.com>
2021-10-28 16:33:06 +0900

    [FE-27] 모아보기 서버랑 연동

--- src/routes/ScrapPage.js
+++ src/routes/ScrapPage.js
@@ -1,100 +1,40 @@
 import produce from "immer";
 import { useEffect, useState } from "react";
+import {
+  getScrappedQuestion,
+  postInterviewListed,
+  postQuestionAnswer,
+  postQuestionScrap,
+} from "../apis/questions";
 import Header from "../common/Header";
 import InterviewSetting from "../components/Scrap/InterviewSetting";
 import ScrapList from "../components/Scrap/ScrapList";
 import "./scrapPage.scss";
 
 function ScrapPage() {
-  const [temp, setTemp] = useState(new Map());
+  const [scrappedQuestions, setScrappedQuestions] = useState(new Map());
 
-  useEffect(() => {
-    let tempttt = new Map();
+  useEffect(async () => {
+    const getScrappedList = await getScrappedQuestion();
 
-    tempttt.set(587, {
-      id: 587,
-      content: "리액트와 뷰의 차이가 무엇인가요?",
-      answer: "이름의 차이입니다 ㅋㅋ",
-      actions: {
-        liked: false,
-        disliked: false,
-        scrapped: true,
-        interviewListed: true,
-        editing: false,
-      },
-    });
+    if (getScrappedList) {
+      let scrappedList = new Map();
 
-    tempttt.set(589, {
-      id: 589,
-      content:
-        "useState가 있는데 왜 보통 Redux, Recoil 등의 프레임워크를 사용하나요?",
-      answer: "ㅁㅁㄴㄴ",
-      actions: {
-        liked: false,
-        disliked: false,
-        scrapped: true,
-        interviewListed: false,
-        editing: false,
-      },
-    });
+      if (getScrappedList.length) {
+        getScrappedList.map((scrapped) => {
+          scrappedList.set(scrapped.id, scrapped);
+          scrappedList.get(scrapped.id).actions.editing = false;
+        });
 
-    tempttt.set(590, {
-      id: 590,
-      content: "useState는 동기인가요, 비동기인가요? 그 이유는 무엇인가요?",
-      answer: "aaa",
-      actions: {
-        liked: false,
-        disliked: false,
-        scrapped: true,
-        interviewListed: true,
-        editing: false,
-      },
-    });
-
-    tempttt.set(591, {
-      id: 591,
-      content: "591번 질문입니다",
-      answer: "이름의 차이입니다 ㅋㅋ",
-      actions: {
-        liked: false,
-        disliked: false,
-        scrapped: true,
-        interviewListed: true,
-        editing: false,
-      },
-    });
-
-    tempttt.set(592, {
-      id: 592,
-      content: "592번 질문입니다",
-      answer: "ㅁㅁㄴㄴ",
-      actions: {
-        liked: false,
-        disliked: false,
-        scrapped: true,
-        interviewListed: false,
-        editing: false,
-      },
-    });
-
-    tempttt.set(593, {
-      id: 593,
-      content: "593번 질문입니다",
-      answer: "aaa",
-      actions: {
-        liked: false,
-        disliked: false,
-        scrapped: true,
-        interviewListed: true,
-        editing: false,
-      },
-    });
-
-    setTemp(tempttt);
+        setScrappedQuestions(scrappedList);
+      }
+    } else {
+      console.log("fail to get scrapped questions");
+    }
   }, []);
 
   function handleChangeAnswer(id, value) {
-    setTemp((prev) =>
+    setScrappedQuestions((prev) =>
       produce(prev, (draft) => {
         draft.get(id).answer = value;
         return draft;
@@ -103,57 +43,47 @@ function ScrapPage() {
   }
 
   function handleEditClick(id) {
-    setTemp((prev) =>
+    if (scrappedQuestions.get(id).actions.editing) {
+      postQuestionAnswer(id, scrappedQuestions.get(id).answer);
+    }
+
+    setScrappedQuestions((prev) =>
       produce(prev, (draft) => {
         draft.get(id).actions.editing = !prev.get(id).actions.editing;
       })
     );
   }
 
-  function handleScrapClick(id) {
-    // setTemp((prev) =>
-    //   produce(prev, (draft) => {
-    //     draft.get(id).actions.scrapped = !prev.get(id).actions.scrapped;
-    //     return draft;
-    //   })
-    // );
-
+  async function handleScrapClick(id) {
     // 눌려있는 스크랩 버튼을 눌렀다 -> 스크랩 하지 않겠다는 뜻
     // 따라서 스크랩 버튼을 누르면 스크랩질문 목록에서 지움
-    setTemp((prev) =>
-      produce(prev, (draft) => {
-        draft.delete(id);
-        return draft;
-      })
-    );
+    if ((await postQuestionScrap(id)).status === 200) {
+      setScrappedQuestions((prev) =>
+        produce(prev, (draft) => {
+          draft.delete(id);
+          return draft;
+        })
+      );
+    }
   }
 
-  function handleAddInterviewClick(id) {
-    setTemp((prev) =>
-      produce(prev, (draft) => {
-        draft.get(id).actions.interviewListed =
-          !prev.get(id).actions.interviewListed;
-        return draft;
-      })
-    );
-  }
-
-  function handleDeleteInterview(id) {
-    console.log("before", temp);
-    setTemp((prev) => {
-      produce(prev, (draft) => {
-        draft.get(id).actions.interviewListed = false;
-        return draft;
-      });
-    });
-    console.log("after", temp);
+  async function handleAddInterviewClick(id) {
+    if ((await postInterviewListed(id)).status === 200) {
+      setScrappedQuestions((prev) =>
+        produce(prev, (draft) => {
+          draft.get(id).actions.interviewListed =
+            !prev.get(id).actions.interviewListed;
+          return draft;
+        })
+      );
+    }
   }
 
   function interviewQuestions() {
     let result = [];
 
-    if (temp) {
-      temp.forEach((question) => {
+    if (scrappedQuestions) {
+      scrappedQuestions.forEach((question) => {
         if (question.actions.interviewListed) {
           result.push({ id: question.id, content: question.content });
         }
@@ -162,10 +92,6 @@ function ScrapPage() {
 
     return result;
   }
-
-  useEffect(() => {
-    console.log("asdfasdf", temp);
-  }, [temp]);
 
   return (
     <>
@@ -180,7 +106,7 @@ function ScrapPage() {
           </div>
           <div className="scrapInterviewWrapper">
             <ScrapList
-              scrapQuestions={temp}
+              scrapQuestions={scrappedQuestions}
               onChangeAnswer={handleChangeAnswer}
               onEditClick={handleEditClick}
               onScrapClick={handleScrapClick}
